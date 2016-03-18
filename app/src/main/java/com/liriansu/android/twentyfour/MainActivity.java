ec71ba32074e9f8b5aecc181bf0a1b4598fa339e
Lirian Su <dg666@163.com>
2016-03-18 14:36:43 +0800

    Make fullscreen

--- app/src/main/java/com/liriansu/android/twentyfour/MainActivity.java
+++ app/src/main/java/com/liriansu/android/twentyfour/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -26,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         divView = (TextView) findViewById(R.id.math_divide);
         minusView = (TextView) findViewById(R.id.math_minus);
